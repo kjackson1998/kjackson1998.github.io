1d8df84ef8fde9078c4ddcefe7f706744de8b5f2
Keith Jackson <kjackson@crimebucket.com>
2023-12-01 22:15:52 -0800

    deploy

--- deploy.ps1
+++ deploy.ps1
@@ -1,15 +1,15 @@
 Remove-Item .\SpaceTraders -Recurse -Force
 mkdir .\SpaceTraders
 
-mkdir .\data
+mkdir -Force .\data
 Copy-Item -Path ..\ShinySpaceTraders\dist\data\systems.json.gz -Destination .\data\
 
-mkdir .\SpaceTraders\game
+mkdir -Force .\SpaceTraders\game
 Copy-Item -Path ..\ShinySpaceTraders\dist\web\prod\game*.* -Destination .\SpaceTraders\game\ -Recurse
 Remove-Item -Force .\SpaceTraders\game\*.map
 Move-Item .\SpaceTraders\game\gameApp.html .\SpaceTraders\game\index.html
 
-mkdir .\SpaceTraders\terminal
+mkdir -Force .\SpaceTraders\terminal
 Copy-Item -Path ..\ShinySpaceTraders\dist\web\prod\terminal*.* -Destination .\SpaceTraders\terminal\ -Recurse
 Remove-Item -Force .\SpaceTraders\terminal\*.map
 Move-Item .\SpaceTraders\terminal\terminalApp.html .\SpaceTraders\terminal\index.html
